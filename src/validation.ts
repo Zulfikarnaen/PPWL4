7902e035e11837de3c4bf2fa58325bba44eb6aaf
Zulfikarnaen <h1101241009@student.untan.ac.id>
2026-03-01 00:26:56 +0700

    menambah endpoint product

--- src/validation.ts
+++ src/validation.ts
@@ -37,6 +37,18 @@ const app = new Elysia()
       })
     }
   )
+
+  app.get("/product/:id",
+  (context) => { return {params: context.params, query: context.query}},
+  {
+    params: t.Object({
+      id: t.Number()
+    }),
+    query: t.Object({
+      sort: t.String({ enum: ["asc", "desc"] })
+    })
+  }    
+  )
   .listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
